cf67e2b88332850bff1e021c318d6b19e5ebe99e
Muller Jorge <millerjmatos@hotmail.com>
2023-02-05 22:28:26 -0300

    script

--- scripts/users.sh
+++ scripts/users.sh
@@ -2,6 +2,14 @@
 
 # Esse script serve para a criação de usuários em lote.
 
+# Inclui um usuário 'master' em cada grupo criado.
+
+# Define uma permissão a cada diretório home dos usuário criados, e que dá ao proprietário e ao grupo 
+# completos privilégios de leitura, escrita e execução no diretório, 
+# mas não permite que outros usuários tenham acesso!
+
+# Além disso, já cria o usuário no Samba e define uma senha padrão.
+
 # Criação de usuários
 password="P@ssword"
 encrypted_password=$(echo "$password" | openssl passwd -1 -stdin)
@@ -10,6 +18,7 @@ users=(gerencia vendas tecnologia)
 for user in "${users[@]}"; do
   useradd -m -p "$encrypted_password" "$user"
   chmod 2770 /home/"$user"
+  smbpasswd -a "$user"
 done
 
 # Adição do usuário muller a todos os grupos
