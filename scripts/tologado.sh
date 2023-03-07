9280aee72d1f613dc198ec710b65fa18ac4795ab
Muller Jorge <millerjmatos@hotmail.com>
2023-03-06 21:42:59 -0300

    script

--- scripts/tologado.sh
+++ scripts/tologado.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #  Verifica se determinado usuário esta "logado".
-#
 if  who | grep $1 > /dev/null
 then
     echo "O $1 está logado!"
 else
     echo "O $1 não esta logado!"
 fi
+
+# Execute:
+
+    # ./tologado.sh root    
